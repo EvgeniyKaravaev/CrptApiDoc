df89796cc32fe952f6b4db9ca59b1a4b38250993
Караваев Евгений <e.karavaev@bftcom.com>
2025-09-14 13:10:18 +0300

    Initial commit

---
+++ src/main/java/com/example/crptapidoc/CrptApiDocApplication.java
@@ -0,0 +1,13 @@
+package com.example.crptapidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrptApiDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrptApiDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crptapidoc/CrptApiDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crptapidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrptApiDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
